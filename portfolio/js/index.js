cd785e7cb15cb011b64c4ba56c83716c1507a5a4
Alderfer <alderfer@oll38888mac-alderfer.ad.garmin.com>
2014-07-24 16:16:01 -0500

    new portfolio started 7/24

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,42 +1,15 @@
-function initializeElements() {
-    /* Act on the event */
-    if ($(window).width() < 992) {
-        $(".panel-lg").css("height", "");
-        $(".panel-sm").height(540);
-        $(".panel-md").height(540);
-    } else {
-        var maxHeight = $(".panel-lg").outerHeight();
-        if (maxHeight < $(window).height()) {
-            maxHeight = $(window).height();
-            $(".panel-lg").outerHeight(maxHeight);
-        }
-        console.log(maxHeight);
-        $(".panels").outerHeight(maxHeight);
-        var newHeight = $(".panels").outerHeight();
-        $(".panel-md").height(newHeight / 2);
-        $(".panel-sm").height(newHeight / 4);
-    }
-}
+var unicornVisible = false;
 
 $(document).ready(function() {
-    initializeElements();
-    $(window).resize(function(event) {
-        /* Act on the event */
-        if ($(window).width() < 992) {
-            $(".panel-lg").css("height", "");
-            $(".panel-sm").height(600);
-            $(".panel-md").height(600);
-        } else {
-            $(".panel-lg").css("height", "");
-            var maxHeight = $(".panel-lg").outerHeight();
-            if (maxHeight < $(window).height()) {
-                maxHeight = $(window).height();
-                $(".panel-lg").outerHeight(maxHeight);
-            }
-            console.log(maxHeight);
-            var newHeight = $(".panels").outerHeight(maxHeight);
-            $(".panel-md").height(newHeight / 2);
-            $(".panel-sm").height(newHeight / 4);
+    $("#unicorn").click(function(event) {
+        if (unicornVisible) {
+            $("#unicorn-gif").slideUp("fast");
+            $(this).html("Designer, Developer,<br/>Entreprenuer");
+            unicornVisible = false;
+        } else if (!unicornVisible) {
+            $("#unicorn-gif").slideDown("fast");
+            $(this).html("Basically a Unicorn");
+            unicornVisible = true;
         }
     });
 });
