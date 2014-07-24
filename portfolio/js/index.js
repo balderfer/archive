299bab767a6ab58ed0047908b1c698bdec177141
Alderfer <alderfer@oll38888mac-alderfer.ad.garmin.com>
2014-07-24 17:00:44 -0500

    nav animation

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,20 +1,25 @@
 var unicornVisible = false;
 
 $(document).ready(function() {
-    $(".nav h2").hover(function(event) {
-        event.stopPropagation();
-        $(this).animate({
-                'margin-left': '10px',
-            },
-            200, "linear"
-        );
-    }, function() {
-        $(this).animate({
-                'margin-left': '0',
-            },
-            200
-        );
-    });
+    var windowSize = window.innerWidth;
+    console.log(windowSize);
+
+    if (windowSize >= 768) {
+        $(".nav h2").hover(function(event) {
+            event.stopPropagation();
+            $(this).animate({
+                    'margin-left': '10px',
+                },
+                200, "linear"
+            );
+        }, function() {
+            $(this).animate({
+                    'margin-left': '0',
+                },
+                200
+            );
+        });
+    }
 
     $(".nav h2").click(function(event) {
         $(this).animate({
