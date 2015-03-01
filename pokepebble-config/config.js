dab1f0da2d670e492f7b04fda50b658d5735f226
Ben <Alderfer@wirelessprvnat-172-17-161-39.near.illinois.edu>
2015-03-01 07:35:44 -0600

    Wat

--- pokepebble-config/config.js
+++ pokepebble-config/config.js
@@ -20,6 +20,14 @@ $(function() {
     config['unique-id'] = uniqueId;
     pokemon = $('#pokemon').val();
     config['pokemon'] = pokemon;
+    var move1 = $('#move1').val();
+    config['move1'] = move1;
+    var move2 = $('#move2').val();
+    config['move2'] = move2;
+    var move3 = $('#move3').val();
+    config['move3'] = move3;
+    var move4 = $('#move4').val();
+    config['move4'] = move4;
 
     var jsconfig = JSON.stringify(config);
     window.localStorage.setItem('config', jsconfig);
